98e61b598a755429c3cc24e40c83590970159ffc
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2016-05-09 18:04:25 +0200

    Anonimizzato e sostituzione nome

--- thingspeak_stat.py
+++ thingspeak_stat.py
@@ -1,23 +1,22 @@
 import time;
-import httplib, urllib #libreria http e ur
-import psutil #libreria per monitorare la macchina.
-
+import httplib, urllib #librerie per il protocollo HTTP
+import psutil          #libreria per monitorare la macchina.
 while True:
-    post = urllib.urlencode (
+    body = urllib.urlencode (
       { "field1": psutil.cpu_percent(),                  # percentuale cpu occupata
         "field2": psutil.virtual_memory().percent,       # memoria usata
         "field3": psutil.net_io_counters().packets_sent, # pacchetti inviati
-        "field4": psutil.net_io_counters().packets_recv, # variabile pacchetti ricevuti
-        "key":"71PZRVYSK1J4FFQ0"}                        # WRITE KEY
+        "field4": psutil.net_io_counters().packets_recv, # pacchetti ricevuti
+        "key":"copia qui la write key"}                        # WRITE KEY
         )
-    print post
+    print body
     headers = {
       "Content-type": "application/x-www-form-urlencoded"
       } 
     conn = httplib.HTTPConnection("api.thingspeak.com:80")
     try:
-        conn.request("POST", "/update", post, headers)
+        conn.request("POST", "/update", body, headers)
         conn.close()
     except:
-        print "Connessione fallita: dato non resistrato." 
+        print "Connessione fallita: dato non registrato." 
     time.sleep(60) 
